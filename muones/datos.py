f45cb2a8d3db93da2fd8217d47a91e3060be02d1
alancovamo <87781171+alancovamo@users.noreply.github.com>
2021-09-09 17:57:32 -0500

    Add files via upload

---
+++ muones/datos.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  9 01:21:10 2021
+
+@author: Alan
+"""
+
+import pandas as pd
+import csv
+import numpy as np      
+#import matplotlib.pyplot as plt
+
+df=pd.read_table("datos_raw.csv",delimiter=',')
+df.rename(columns = {df.columns[0]:'datos'},inplace=True)
+
+df=df['datos'].str.split('  ',n=12,expand=True)
+#print(df.head())
+
+f=df[12].str.split(' ',n=7,expand=True)
+f=f.drop(df.columns[0],axis=1)
+#print(f)
+
+g1= df.filter([3]).copy()
+g1.rename(columns={g1.columns[0]: 0},inplace=True)
+g2=f.filter([1,2,3,4,5,6,7]).copy()
+g=g1.join(g2)
+
+#print(g.head())
+
+g = g.astype(float)
+g.rename(columns= {g.columns[0]: 'masa',
+                   g.columns[1]: 'momento en z',
+                   g.columns[2]: 'momento transversal',
+                   g.columns[3]: 'Dist. angular $\phi$',
+                   g.columns[4]: 'Dist. angular $\eta$',
+    },inplace=True)
+g.to_csv('datosprocesados.csv')
+
+print(g.head())
+
+
+#plt.figure(0)
+#g.hist(column=['momento en z'])
+#plt.show()
+#plt.plot.hist(column2)
