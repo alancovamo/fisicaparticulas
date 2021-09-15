3d7e1371f85e343106dc799ccd22e81fb1bdc562
Alan <87781171+alancovamo@users.noreply.github.com>
2021-09-15 17:24:48 -0500

    Solo sirve para mi dataset
    Este archivo solo sirve para mi dataset, para usarlo con el de la doctora Isabel se necesita modificar.

---
+++ muones/misdatos/juntarmumu.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 14 19:17:22 2021
+
+@author: Alan
+"""
+
+import pandas as pd
+import csv
+import numpy as np      
+import matplotlib.pyplot as plt
+
+df = pd.read_table('momento.csv',delimiter=',')
+df_1=df.iloc[lambda x: x.index % 2 == 0]
+df_1=df_1.drop(df_1.columns[0],axis=1)
+df_1=df_1.reset_index(drop=True)
+df_2=df.iloc[lambda x: x.index % 2 != 0]
+df_2=df_2.drop(df_2.columns[0],axis=1)
+df_2=df_2.reset_index(drop=True)
+df_2.rename(columns= {df_2.columns[0]:'masa*',
+                      df_2.columns[1]: 'px*',
+                      df_2.columns[2]: 'py*',
+                      df_2.columns[3]: 'pz*',
+                      df_2.columns[4]: 'E*',
+                      },inplace=True)
+
+df_3=df_1.join(df_2)
+print(df_3)
+df_3.to_csv('mu+mu-.csv')
+
